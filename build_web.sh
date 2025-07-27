e20c7d57f2e0c74634cca05d2a704ba2b30232e7
flebedev77 <flebedev99@gmail.com>
2025-07-27 19:33:16 +0930

    Web build script

---
+++ build_web.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "[Cleaning]"
+make clean
+echo "[Building]"
+make CC=emcc \
+  CFLAGS="-Wall -Wextra -O3" \
+  LINK_FLAGS="-lSDL2 --embed-file testroms -s SINGLE_FILE -s ASYNCIFY" \
+  TARGET="build/index.html"
+if [ $? != 0 ]; then
+  echo "[Build failed]"
+  exit 1
+fi
+echo "[Starting web server]"
+python3 -m http.server -d build
